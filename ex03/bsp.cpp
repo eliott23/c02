c641d2ea7f297de70d08145844c43b193a1e595f
eliott23 <62487535+eliott23@users.noreply.github.com>
2023-04-18 09:57:05 +0000

    check

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -10,7 +10,7 @@ Fixed   de(const Point& a, const Point& b)
 bool bsp( const Point a, const Point b, const Point c, const Point p)
 {
     Fixed Da = (de(p, c) - de(a, b)) / de(a, c);
-    Fixed Db = (de(p, b) - de(a, c)) / de(a, c);
+    Fixed Db = (de(a, c) - de(p, b) ) / de(a, c);
     // if (Da > 0 && Db > 0 && (Db + Da) < 1)
     //     return true;
     return false;
